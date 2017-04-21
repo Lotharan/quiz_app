ff843751a78f4320b618d6b5a9ccb66ed1dd6960
Lotharan <dklotharan@hotmail.com>
2017-04-20 23:02:04 -0400

    created object

---
+++ app.js
@@ -0,0 +1,22 @@
+var quizQuestions = [{
+		question: "What is the name of the largest star currently known?",
+		choices: ['VY Canis Majoris', 'UY Scuti', 'NML Cygni', 'Betelgeuse'],
+		correctAnswer: 'UY Scuti'
+	}, {
+		question: "How many galaxies are there estimated to be in the universe?",
+		choices: ['100 Billion', '200 Billion', '1 Trillion', '2 Trillion'],
+		correctAnswer: '2 Trillion'
+	}, {
+		question: "What is the name of the nearest Galaxy to the Milky Way?",
+		choices: ['Pinwheel', 'Triangulum', 'Starburst', 'Andromeda'],
+		correctAnswer: 'Andromeda'
+	}, {
+		question: "What is the radius of the observable universe?",
+		choices: ['46.6 billion light years', '13.8 billion light years', '22.4 billion light years', '10.3 billion light years'],
+		correctAnswer: '46.6 billion'
+	}, {
+		question: "How many light years across is the milky way galaxy?",
+		choices: ['25,000', '50,000', '75,000', '100,000'],
+		correctAnswer: '100,000'
+	}];
+}]
